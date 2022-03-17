ff0e4acee7aa7757b944d298ab1cebdf18ce7f82
Svetlana <cecastosic@gmail.com>
2022-03-17 09:34:59 +0100

    exercises week 1

---
+++ 1/1.js
@@ -0,0 +1,54 @@
+// EXERCISE A
+let a = ["22", "45", "54", "87"];
+let b = ["22", "45", "87"]
+
+// write algorithm that can find out if array ‘b’ is in array ‘a’
+const checkArray = () => {
+    if (a.length < b.length) return false;
+    //return b.map(el => a.includes(el)).reduce((sum, el) => sum + el, true);
+    return b.every(el => a.includes(el));
+}
+console.log(checkArray());
+
+
+// EXERCISE B
+// f0 = 1
+// f1 = 1
+// f2 = f1 + f0
+// find out what is x5
+const arr = [1, 1];
+
+console.time("First function");
+const nInTheArr = (ind) => {
+    for(let i = 2; i <= ind; i++) {
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+    return arr[ind];
+};
+console.timeEnd("First function");
+
+console.time("Second function");
+const nInTheArrSlow = (ind) => {
+    if (ind < 2) {
+        return 1;
+    }
+    else {
+        return nInTheArrSlow(ind-1) + nInTheArrSlow(ind - 2);
+    }
+}
+console.timeEnd("Second function");
+
+console.log(nInTheArr(5));
+console.log(nInTheArrSlow(5));
+// make algorithm that can find ‘f’ for any numbers up to 50;
+// describe the computational complexity of the solution
+// test the speed of these algorithms
+
+
+
+// EXERCISE C
+// write HTML that shows message "This is the message" and two buttons
+// message spans across the window and underneat that are two buttons.
+// 'cancel' button is aligned to left and 'ok' button is aligned to right
+// build this exercise without GRID or FLEX, with GRID, with FLEX
+// find out browser support for your solutions.
